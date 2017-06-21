a15282f028d11d2ba0f545fca43b5010c140d0db
lissychu <lissychu@gmail.com>
2017-06-21 20:55:40 +0300

    Added function to generate deck

--- Task #1.py
+++ Task #1.py
@@ -2,8 +2,8 @@ suit = ['s', 'c', 'd', 'h']
 rang = ['6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']
 def new_deck () :
     deck = []
-    for m in suit:
-           for l in rang:
+    for m in suit :
+           for l in rang :
                 card = m + l
                 deck.append (card)
     return deck

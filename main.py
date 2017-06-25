e2f77cef9f7bd506c96564aa984f946b0251a866
MykhayloSharapov <mikhailsharapov76@gmail.com>
2017-06-25 10:25:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,6 @@ def create_new_deck():
 def shuffle_deck(deck):
     random.shuffle(deck)
 
-# Took MykhayloSharapov code and wrote it as a function
-def trump():
-    t=random.randint(0,3)
-    trump = SUITS[t]
-    return trump
-print('Trump is',trump())
-
-
-
 # players - number of players
 players = 5
 
@@ -47,4 +38,35 @@ print(deck)
 print(len(deck))
 shuffle_deck(deck)
 print(deck)
+
+#Issue 5: select Trump        
+
+def elect_trump():
+    trump = str(deck[35])[0]
+    return str(trump)
+
+trump = elect_trump()
+print ('Trump is', trump)
+
+# Issue 6: Determine first to play.
+# Let's arrange the deck in the order of human search for the smallets trump on hands.
+
+trumped_SUITS = SUITS[:]
+
+i = 0
+while trumped_SUITS [i] != trump:
+        trumped_SUITS.append (trumped_SUITS [i])
+        trumped_SUITS.pop (i)
+
+trumped_deck = []
+
+for i in (trumped_SUITS):
+    for j in (RANKS):
+        trumped_deck.append (i+j)
+
+i = 0
+while trumped_deck[i] not in (players_cards):
+    i+=1
+
+print ('First to go is player No',int((players_cards.index (trumped_deck[i])+1)//6.001+1),'!')
 
